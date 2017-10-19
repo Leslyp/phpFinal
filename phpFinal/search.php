2693cf83f8ff590f4a62efbc821b6d67c356dd22
Lesly Perez <leslyperez@Administrators-MacBook-Pro-3.local>
2017-10-18 21:24:53 -0400

    fix search page

--- phpFinal/search.php
+++ phpFinal/search.php
@@ -1,43 +1,57 @@
 <?php 
-	// section variable lets us find which page is selected to show that it's selected on nav
-  $section = "products";
+  // section variable lets us find which page is selected to show that it's selected on nav
+	$section = "search";
 	include("./lib/inc/header.php");
 
-	// add to all variables
-	$search = !empty($_GET['search']) ? $_GET['search'] : '';
+	$price = !empty($_GET['filterPrice']) ? $_GET['filterPrice'] : '';
+	$term = !empty($_GET['searchTerm']) ? $_GET['searchTerm'] : '';
 
-	// calling method in pet products class to return results
+	// fetch all categories to form select dropdown
 	$categories = PetProducts::get_all_categories();
-  
-  if (empty($search) || $search == "Browse All") { // show all products
-  	$products = PetProducts::get_all_products();
-  } else {  //show products that match the user color selected
-  	$products = PetProducts::get_products_by_category($search);
-  }
+
+	if (isset($_GET['submit'])) {  
+		// use switch statement to check price value to know which select function to execute 
+		switch ($price) {
+			case 'low':
+				$products = PetProducts::get_products_by_search_low($term);
+				break;
+			case 'medium':
+				$products = PetProducts::get_products_by_search_medium($term);
+				break;
+			case 'high':
+				$products = PetProducts::get_products_by_search_high($term);
+				break;
+			default:
+				$products = PetProducts::get_products_by_search_all($term);
+				break;
+		}
+	} 
 
 ?>
 
-<main class="tableContainer">
-	<h1>Products</h1>
-	<div class="formContainer">
-		<form id="searchForm" method="GET" action="products.php">
-			<label for="search">Category</label>
-			<!-- create select dropdown with mysql data -->
-			<select id='search' name='search'>
-			<option>Browse All</option>
-				<?php foreach($categories as $category): ?>
-					<!-- add selected to option to show it in dropdown -->
-					<option <?= ($search == $category['category'] ?'selected' : '') ?> value='<?= $category['category'] ?>'>
-					<?= $category['category'] ?>
-					</option>
-				<?php endforeach; ?>
+<main>
+	<h1>Search For Item</h1>
+	<form id="searchItemForm" action="search.php" method="GET">
+		<div>
+			<label for="searchTerm">&#128269; </label>
+			<input type="text" id="searchTerm" name="searchTerm" placeholder="Search For Product..." value="<?php echo htmlspecialchars($term); ?>">
+		</div>
+		<div>
+			<label for="filterPrice">Select Price</label>
+			<select id="filterPrice" name="filterPrice">
+				<option <?= ($price == "all" ?'selected' : '') ?> value="all">Browse All Prices</option>
+				<option <?= ($price == "low" ?'selected' : '') ?> value="low">$8 - $16</option>
+				<option <?= ($price == "medium" ?'selected' : '') ?> value="medium">$20 - $25</option>
+				<option <?= ($price == "high" ?'selected' : '') ?> value="high">$30 - $40</option>
 			</select>
-			<div class="submitBtn">
-				<button type='submit'>Search</button>
-			</div>
-		</form>
-	</div>
-  <div class="new">
+		</div>
+		<div class="submitBtn">
+			<button type="submit" name="submit" value="Submit">Submit</button> 
+		</div>
+	</form>
+	<div class="new">
+		<!-- use if statement to show products if there are any -->
+		<?php if (isset($_GET['submit']) && !empty($products)): ?>
 		<?php foreach($products as $product): ?>
 			<figure class="figures">
 				<a href='productDetails.php?productId=<?= $product['productId'] ?>'>
@@ -49,8 +63,13 @@
 					<p>$<?= $product['price'] ?></p>
 				</figcaption>
 			</figure>
-    <?php endforeach; ?>
-  </div>
+	  <?php endforeach; ?>
+		<?php endif; ?>
+		<!-- use if statement to show error message if there aren't any products -->
+		<?php if (isset($_GET['submit']) && empty($products)): ?>
+	  <p id="noResults"><?= 'No results found, please enter another term.'; ?></p>
+		<?php endif; ?>
+	</div>
 </main>
-<?php include("./lib/inc/footer.php"); ?>
 
+<?php include("./lib/inc/footer.php"); ?>
